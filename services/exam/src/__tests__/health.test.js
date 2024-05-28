8d84f9861abbdc1cf5a69ceff23caa4b42cf8936
arif-btkrm <arif.btkrm@gmail.com>
2024-05-28 19:03:08 +0600

    Exam error Fixed

--- services/exam/src/__tests__/health.test.js
+++ services/exam/src/__tests__/health.test.js
@@ -5,6 +5,6 @@ describe('Health Route', () => {
   it('should respond with status 200 and a health message', async () => {
     const response = await request(app).get('/health');
     expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Hello from Auth-Service and Health Ok');
+    expect(response.body.message).toBe('Hello from Exam-Service and Health Ok');
   });
 });
